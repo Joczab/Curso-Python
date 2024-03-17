d36456b64708227a81d400000f5e959be8b633bb
joc <josedanielzabala@gmail.com>
2024-03-17 11:46:50 -0400

    Update ProyectoClase1.py

---
+++ Clase 1/ProyectoClase1.py
@@ -0,0 +1,33 @@
+estudiante1 = {
+    'name ':' Jose ',
+    'lastname': 'Zabala',
+    'cedula': 29691806,
+    'nota1': 10,
+    'nota2': 10,
+    'nota3': 10,
+    'promedio': 10
+}
+estudiante2 = {
+    'name ':' Jesus',
+    'lastname': 'Leal',
+    'cedula': 29999470,
+    'nota1': 10,
+    'nota2': 10,
+    'nota3': 10,
+    'promedio': 10    
+}
+estudiante3 = {
+    'name ':' Ruben',
+    'lastname': 'Valbuena',
+    'cedula': 281234567,
+    'nota1': 10,
+    'nota2': 10,
+    'nota3': 10,
+    'promedio': 10
+}
+
+
+estudiantes = [estudiante1,estudiante2,estudiante3]
+
+print('Gestion de Estudiante Universitario')
+
