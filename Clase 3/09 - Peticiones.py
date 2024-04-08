a83d05147845ba01591842daf2c348f5f6105d78
joc <josedanielzabala@gmail.com>
2024-04-07 20:25:04 -0400

    clase 3

--- Clase 3/09 - Peticiones.py
+++ Clase 3/09 - Peticiones.py
@@ -1,9 +1,10 @@
-# instalar despues py -m pip install request  
-'''import requests
+# instalar despues py -m pip install request 
+
+import requests
 import json
 
 def obtener_personajes():
-    request = requests.get(url=(aca va la url de la api de rick and morty) )
+    request = requests.get(url=('https://rickandmortyapi.com/api'))
     print(request)
     
     if request.status_code == 404:
@@ -12,14 +13,16 @@ def obtener_personajes():
     data = request.json()
     
     crear_json(data)
+    return data
 
-def crear_json(data):
+def crear_json(data): 
+    
     with open('./rick_and_morty.json','w',encoding='utf-8') as jsonfile:
         
         json.dump(data, jsonfile,indent=4)
         
 if __name__ == '__main__':
     
-    obtener_personajes()'''
-    
+    data = obtener_personajes()
+    print(data)
     
